edfd2f6e3f8a78281b4717d246e330b1d5261be6
Catherine Gichina <catherinegichinaa@gmail.com>
2025-01-01 22:09:52 +0300

    Initial commit

---
+++ my-react-info-site/src/App.jsx
@@ -0,0 +1,15 @@
+import Navigation from './Navigation.jsx'
+import Content from './Content.jsx'
+
+function App() {
+  return(
+    <div>
+  <Navigation></Navigation>
+  <Content></Content>
+    </div>
+    
+  
+  )
+}
+
+export default App
